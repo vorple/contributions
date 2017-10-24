ebe505495518cedc1da20a99bf4e484d168eb599
hugo <hugo@hlabrande.fr>
2017-10-24 22:58:39 +0200

    HoveringFillsCommandLine: Added usage and removed unneeded dependency

--- i6-extensions/HoveringFillsCommandLine.h
+++ i6-extensions/HoveringFillsCommandLine.h
@@ -6,9 +6,11 @@
 ! This simple extension recreates a very cool effect I first saw in No Mires, a Spanish game by Edlobez, written in Java. I believe that game won XComp3, a competition for games which experiment with interfaces!
 ! The function "HoverFillLink" takes two arguments: the text, and the command. The text is displayed in a hyperlink; when the player hovers over the link, the command line fills with the command, and clicking on the link enters the command.
 
+! Usage :
+!   print "You can see a "; HoverFillLink("torch", "take torch"); print " here";
+
 Include "vorple.h";
 Include "vorple-hyperlinks.h";
-Include "vorple-command-prompt-control.h";
 
 [ HoverFillLink text cmd classes   id ;
     if (cmd == 0) { cmd = text; }
